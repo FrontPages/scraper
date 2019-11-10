f44274de9fa5f3db676f36184ccb1878e8f2431d
Jesse Pinho <jesse@jessepinho.com>
2019-11-10 11:18:21 +0100

    feat: scrape headlines

--- scraper.ts
+++ scraper.ts
@@ -12,20 +12,34 @@ export const handler = async (site: Site, context: AWSLambda.Context) => {
       defaultViewport: chromium.defaultViewport,
       executablePath: await chromium.executablePath,
       headless: chromium.headless,
-    });
+    })
 
-    let page = await browser.newPage();
-    await page.goto(site.url);
+    let page = await browser.newPage()
+    await page.setViewport({
+      width: 1440,
+      height: 900,
+      deviceScaleFactor: 1,
+    })
+    await page.goto(site.url)
 
-    result = await page.title();
+    const headlineElements = await page.$$(site.selector)
+    const headlines = await Promise.all(
+      headlineElements.map(headlineElement =>
+        headlineElement.evaluate(node => node.textContent)
+      )
+    )
+    console.log(`Headlines: ${JSON.stringify(headlines, null, 2)}`)
+    const screenshot = await page.screenshot({ fullPage: true, encoding: 'base64' })
+    console.log(screenshot)
+    result = await page.title()
     console.log(`Page title: ${result}`)
   } catch (error) {
-    return context.fail(error);
+    return context.fail(error)
   } finally {
     if (browser !== null) {
-      await browser.close();
+      await browser.close()
     }
   }
 
-  return context.succeed(result);
-};
+  return context.succeed(result)
+}
